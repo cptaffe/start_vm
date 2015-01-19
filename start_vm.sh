cb217155c328074f118e4ef345edde668fdb8ebe
Connor Taffe <cpaynetaffe@gmail.com>
2015-01-18 21:21:13 -0600

    greatly improved

--- start_vm.sh
+++ start_vm.sh
@@ -5,15 +5,15 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-# set -o xtrace
+set -o xtrace
 
 # print usage statistics on bad invocation.
 usage() {
-	echo "${0} vm_name"
+	echo "${0} [-s vm_name | -l]"
 }
 
 # check if vm of name (first argument) is running.
-check_vm() {
+vm_running() {
 	# check args
 	if test "${#}" -eq 1; then
 		# test if running
@@ -23,17 +23,45 @@ check_vm() {
 			echo 0 # false
 		fi
 	else
-		usage
+		echo "incorrect invocation of routine 'vm_running'."
+		exit 1
+	fi
+}
+
+vm_exists() {
+	# check args
+	if test "${#}" -eq 1; then
+		# test if running
+		if test -n "$(VBoxManage list vms | grep -e "^\"${1}\"")"; then
+			echo 1 # true
+		else
+			echo 0 # false
+		fi
+	else
+		echo "incorrect invocation of routine 'vm_exists'."
 		exit 1
 	fi
 }
 
 # if vm of name (first argument) is not running, start it.
-if test "${#}" -eq 1; then
-	if test $(check_vm "${1}") -eq 0; then
-		VBoxManage startvm "${1}" --type headless
+start_vm() {
+	if test "${#}" -eq 1; then
+		if $(test $(vm_running "${1}") -eq 0) && $(test $(vm_exists "${1}") -eq 1); then
+			VBoxManage startvm "${1}" --type headless
+		elif test $(vm_running "${1}") -eq 1; then
+			echo "vm is already running"
+		else
+			echo "vm does not exist"
+		fi
+	else
+		echo "incorrect invocation of routine 'start_vm'."
+		exit 1
 	fi
-else
-	usage;
-	exit 1
+}
+
+# check arguments for correct action
+if $(test "${1}" == "-s") && $(test "${#}" -eq 2); then
+	start_vm "${2}"
+elif $(test "${1}" == "-l") && $(test "${#}" -eq 1); then
+	VBoxManage list vms
 fi
